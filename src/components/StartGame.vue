4d44b752cba56d90d0a7975e85448da1da1a6d9a
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-04-06 02:14:15 +0200

    adding copy to clipboard

--- src/components/StartGame.vue
+++ src/components/StartGame.vue
@@ -1,15 +1,33 @@
 <template>
   <form>
+    <br/>
     <div class="field">
       <label class="label">Solution</label>
       <div class="control">
         <input ref="solution" class="input" type="text" placeholder="Insert solution" v-model='solution'>
       </div>
     </div>
+    <br/>
     <div class="field">
       <label class="label">Answer</label>
       <div class="control">
         <input ref="answer" class="input" :class="{'is-danger': error}" type="text" placeholder="Insert answer" v-model='answer' @keyup="on_answer_input">
+      </div>
+    </div>
+
+    <div class="field">
+      <label class="label">Share the URL!</label>
+      <div class="control">
+        <div class="field has-addons">
+          <div class="control is-expanded">
+            <input ref="url" class="input" type="text" :value='absolute_url'>
+          </div>
+          <div class="control">
+            <button class="button is-info" @click='on_copy_click'>
+              COPY
+            </button>
+          </div>
+        </div>
       </div>
     </div>
     <br/>
@@ -30,20 +48,18 @@
 <script>
 
 import {dumps} from '../utils/encode'
-import emojiRegex from 'emoji-regex'
-
-const regex = emojiRegex();
+import copy from 'copy-to-clipboard'
 
 export default {
   name: 'HelloWorld',
   data () {
     return {
       stokazzo: 2,
-      solution: 'stokazzo',
+      solution: 'Hotel California',
       answer: '😂😭',
       proposals: {
         'pompiere':   '😅🤩',
-        'salvini':    '🤡',
+        'IT':    '🤡',
         'di maio':    '🤟🏼',
         'cicciolina': '😂😭',
       }
@@ -55,11 +71,19 @@ export default {
     },
     on_answer_input: function() {
       this.answer = this.filtered
+    },
+    on_copy_click: function() {
+      copy(this.absolute_url);
     }
   },
   computed: {
     url: function() {
       return this.get_url(this.solution, this.answer)
+    },
+    absolute_url: function() {
+      const url = document.createElement('a')
+      url.setAttribute('href', this.url)
+      return url.href
     },
     filtered: function() {
       return  this.answer.replace(/[\u2190-\u21FF]|[\u2600-\u26FF]|[\u2700-\u27BF]|[\u3000-\u303F]|[\u1F300-\u1F64F]|[\u1F680-\u1F6FF]/g, "").trim();
