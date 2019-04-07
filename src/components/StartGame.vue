720bc9324ef081b91762b880c471eb637020f254
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-04-07 09:23:30 +0200

    adding search and suggestions

--- src/components/StartGame.vue
+++ src/components/StartGame.vue
@@ -9,7 +9,7 @@
       <div class="control">
         <div class="field has-addons">
           <div class="control is-expanded">
-            <input ref="solution" class="input has-text-centered" type="text" placeholder="Psyco" v-model='solution'>
+            <input ref="solution" class="input has-text-centered" type="text" :placeholder="solution_suggestion" v-model='solution'>
           </div>
           <div class="control">
             <button class="button is-static">
@@ -17,9 +17,16 @@
             </button>
           </div>
           <div class="control">
-            <input ref="answer" class="input has-text-centered" :class="{'is-danger': invalid_input}" type="text" v-model='answer' @keyup="on_answer_input">
+            <input ref="answer" class="input has-text-centered" :placeholder="answer_suggestion" :class="{'is-danger': invalid_input}" type="text" v-model='answer' @blur="on_answer_blur">
           </div>
         </div>
+      </div>
+    </div>
+    <div class="emoji-controls">
+      <div class="buttons">
+        <a class="button is-small" :title='name' v-show='is_emoji_visible(emoji)' v-for='emoji, name in emojis' @click.prevent='on_click_emoji(emoji)'>
+          <span class="icon is-small">{{ emoji }}</span>
+        </a>
       </div>
     </div>
     <br/>
@@ -48,15 +55,16 @@
 
 <script>
 
+import {remove_text, remove_emoji, normalize_solution} from '../utils/text'
 import {answer_url}    from '../utils/urls'
-import {remove_emojis} from '../utils/text'
+import emoji           from '../utils/emoji'
 
-
-//import emoji          from '../utils/emoji'
-import copy           from 'copy-to-clipboard'
-import map            from 'rfuncs/functions/map'
-import length         from 'rfuncs/functions/length'
-import values         from 'rfuncs/functions/values'
+import copy            from 'copy-to-clipboard'
+import map             from 'rfuncs/functions/map'
+import length          from 'rfuncs/functions/length'
+import values          from 'rfuncs/functions/values'
+import first           from 'rfuncs/functions/first'
+import filter          from 'rfuncs/functions/filter'
 
 export default {
   name: 'HelloWorld',
@@ -64,44 +72,73 @@ export default {
     return {
       solution: '',
       answer: '',
-      //emoji: emoji.emoji
+      emojis: emoji.emoji,
+      is_searching: false
     }
   },
   methods: {
-    on_answer_input: function() {
-      this.answer = this.filtered
+    on_answer_blur: function() {
+      setTimeout(
+        () => this.answer = this.filtered,
+        125
+      )
     },
     on_copy_click: function() {
       copy(this.absolute_url);
     },
-    //is_emoji_visible: function(emoji) {
-    //  return this.search_results[emoji]
-    //}
+    on_click_emoji: function(emoji) {
+      this.answer = this.filtered + emoji
+    },
+    is_emoji_visible: function(emoji) {
+      if (length(this.emoji_query)) {
+        return this.emoji_query[emoji]
+      }
+      return true
+    }
   },
   computed: {
     url: function() {
-      return answer_url(this.solution, this.answer)
+      return answer_url(
+        this.solution || this.solution_suggestion, 
+        this.answer   || this.answer_suggestion,
+      )
     },
     absolute_url: function() {
       const url = document.createElement('a')
       url.setAttribute('href', '/')
       return url.href.slice(0, -1) + this.url
     },
+
     filtered: function() {
-      return  remove_emojis(this.answer)
+      return remove_text(this.answer)
     },
     invalid_input: function() {
       return this.filtered !== this.answer
     },
     invalid_url: function() {
-      return ! this.answer || ! this.solution
+      return false
     },
-    //search_results: function() {
-    //  return emoji.search(this.solution)
-    //},
-    //suggestion: function() {
-    //  return map(d => d.emoji, values(this.search_results)).join('')
-    //}
+
+    emoji_query: function() {
+      return emoji.search(remove_emoji(this.answer))
+    },
+    search_results: function() {
+      return emoji.search(this.solution)
+    },
+
+    solution_suggestion: function() {
+      return 'apple'
+    },
+    answer_suggestion: function() {
+
+      const query  = normalize_solution(this.solution || this.solution_suggestion)
+      const words  = query.split(' ')
+
+      if (! query) {return ''}
+
+      return map(d => d.emoji, filter(a => a, map(w => first(emoji.search(w)), words))).join('')
+
+    }
   },
   mounted(){
     this.$refs.solution.focus();
@@ -123,6 +160,11 @@ li {
 
 form {
 
+}
+
+.emoji-controls {
+  max-height: 138px;
+  overflow-y: scroll;
 }
 
 </style>
