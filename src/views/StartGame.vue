5d66a672576bfa8bfc1e5f3708e175ec2952e8d5
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-04-07 20:19:18 +0200

    smaller margin

--- src/views/StartGame.vue
+++ src/views/StartGame.vue
@@ -145,7 +145,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 .emoji-controls {
-  height: 138px;
+  height: 128px;
   overflow-y: scroll;
 }
 </style>
