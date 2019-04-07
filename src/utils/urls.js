b38cb2830de12ffc30fa33c94219162c012f2559
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-04-07 08:41:40 +0200

    doing normalization in urls

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,8 +1,9 @@
 
 import {dumps} from './base64'
+import {remove_emojis, normalize_solution} from './text'
 
 export function answer_url(solution, answer) {
-    return '/' + (answer || ' ').replace(' ', '+') + '/' + dumps(solution || '')
+    return '/' + remove_emojis(answer || '🤷').replace(' ', '+') + '/' + dumps(normalize_solution(solution || ''))
 }
 
 export default {answer_url}
