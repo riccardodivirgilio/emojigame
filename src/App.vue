9df3d1442c5f5f070ea0e5c673a7a9464171232f
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-04-07 17:24:38 +0200

    improving mobile ux

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,30 @@
 <template>
-  <section class="section">
-    <div class="container">
-      <div class="columns">
-        <div class="column"></div>
-        <div class="column is-half">
-          <div class="card">
-            <header class="card-header">
-              <router-link to="/" class="card-header-title" style="text-align: center; display: block; font-size: 35px">The Emoji Game</a></router-link>
-            </header>
-            <router-view/>
-          </div>
-          <footer class="has-text-grey has-text-centered">
-            <br/>
-            <small>
-                Made with ❤️ in 🇮🇹
-                by <a href='https://github.com/riccardodivirgilio' target='_blank'>@riccardodivirgilio</a>
-                and <a href='https://github.com/andreacat' target='_blank'>@pinoli</a>.
-            <br/>
-            Follow on <a href='https://github.com/riccardodivirgilio/emojigame' target='_blank'>Github</a>.
-            </small>
-          </footer>
+  <div class="app container">
+    <div class="columns">
+      <div class="column"></div>
+      <div class="column is-half">
+        <br class='is-hidden-mobile'/>
+        <br class='is-hidden-mobile'/>
+        <div class="card">
+          <header class="card-header">
+            <router-link to="/" class="card-header-title" style="text-align: center; display: block; font-size: 35px">The Emoji Game</a></router-link>
+          </header>
+          <router-view/>
         </div>
-        <div class="column"></div>
+        <footer class="has-text-grey has-text-centered">
+          <br/>
+          <small>
+              Made with ❤️ in 🇮🇹
+              by <a href='https://github.com/riccardodivirgilio' target='_blank'>@riccardodivirgilio</a>
+              and <a href='https://github.com/andreacat' target='_blank'>@pinoli</a>.
+          <br/>
+          Follow on <a href='https://github.com/riccardodivirgilio/emojigame' target='_blank'>Github</a>.
+          </small>
+        </footer>
       </div>
+      <div class="column"></div>
     </div>
-  </section>
-
+  </div>
 </template>
 
 <script>
@@ -38,12 +37,12 @@ export default {
 
 $primary: #3273dc
 $body-background-color: #f9f9f9
-
-
-
 $progress-bar-background-color: white
 
 @import 'bulma/bulma.sass'
+
+.app > .columns > .column 
+  padding-top: 0px
 
 .logo a
   color: black
