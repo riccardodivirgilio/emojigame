bbe4f7fdc460f9b9ee78409c48b105d4c1abe813
Riccardo Di Virgilio <riccardod@wolfram.com>
2019-04-07 21:24:26 +0200

    showing levels

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,26 @@
           </header>
           <router-view/>
         </div>
-        <footer class="has-text-grey has-text-centered">
-          <br/>
-          <small>
-              Made with ❤️ in 🇮🇹
-              by <a href='https://github.com/riccardodivirgilio' target='_blank'>@riccardodivirgilio</a>
-              and <a href='https://github.com/andreacat' target='_blank'>@andreacat</a>.
-          <br/>
-          Follow on <a href='https://github.com/riccardodivirgilio/emojigame' target='_blank'>Github</a>.
-          </small>
+        <footer class="footer">
+          <div class="content has-text-centered">
+            <p>
+              <small>
+                  Made with ❤️ in 🇮🇹
+                  by <a href='https://github.com/riccardodivirgilio' target='_blank'>@riccardodivirgilio</a>
+                  and <a href='https://github.com/andreacat' target='_blank'>@andreacat</a>.
+              </small>
+            </p>
+            <p>
+              Follow on <a href='https://github.com/riccardodivirgilio/emojigame' target='_blank'>Github</a>.
+              </small>
+            </p>
+            <br/>
+            <div class="buttons is-centered">
+              <router-link :to='answer_url(solution, emoji)' class="button " v-for='emoji, solution in examples'>
+                {{ emoji }}
+              </router-link>
+            </div>
+          </div>
         </footer>
       </div>
       <div class="column"></div>
@@ -28,8 +39,20 @@
 </template>
 
 <script>
+
+import examples     from '@/utils/examples'
+import {answer_url} from '@/utils/urls'
+
 export default {
-  name: 'EmojiGame'
+  name: 'EmojiGame',
+  data () {
+    return {
+      examples: examples
+    }
+  },
+  methods: {
+    answer_url: answer_url
+  }
 }
 </script>
 
